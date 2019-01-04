c3955a3aa6769e05aba6948714d3cafce43a01bd
Gowtham Behara <gothsiddu@gmail.com>
2019-01-04 13:26:55 +0530

    Add RequiredArgsContructor annotation and rename attribute to postalCode

--- src/main/java/com/gowtham/jugtours/model/Group.java
+++ src/main/java/com/gowtham/jugtours/model/Group.java
@@ -1,16 +1,13 @@
 package com.gowtham.jugtours.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Entity
 @Table(name = "user_group")
 public class Group {
@@ -24,7 +21,7 @@ public class Group {
     private String city;
     private String state;
     private String country;
-    private String postalCOde;
+    private String postalCode;
     @ManyToOne(cascade = CascadeType.PERSIST)
     private User user;
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
