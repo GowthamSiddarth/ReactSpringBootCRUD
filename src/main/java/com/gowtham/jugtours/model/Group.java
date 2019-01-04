219bc97191597c5b867ce828cb3ec63913c8d5cc
Gowtham Behara <gothsiddu@gmail.com>
2019-01-04 11:45:09 +0530

    Create model for Group

---
+++ src/main/java/com/gowtham/jugtours/model/Group.java
@@ -0,0 +1,33 @@
+package com.gowtham.jugtours.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "user_group")
+public class Group {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NonNull
+    private String name;
+    private String address;
+    private String city;
+    private String state;
+    private String country;
+    private String postalCOde;
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    private User user;
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<Event> events;
+
+}
